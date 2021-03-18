90a1f0f715230810d6e1b356f666ee2f83a48150
Kingstar <70220191+KingstarLeak@users.noreply.github.com>
2021-03-18 20:54:57 +0100

    Add files via upload

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,5 @@
-console.log("Lancement de la page.")
-console.log(" ")
-console.log("CODE SOUEN DANS LA BOUTIQUE POUR UN MAX DE NO SCOPE")
-console.log("Quand il y aura le sniper qui sera de retour")
-var souen = "#freesouen"
+console.log("Lancement de la page.");
+console.log(" ");
+console.log("CODE SOUEN DANS LA BOUTIQUE POUR UN MAX DE NO SCOPE");
+console.log("Quand il y aura le sniper qui sera de retour");
+var souen = "#freesouen";
